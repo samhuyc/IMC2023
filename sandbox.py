996bd02680ec6e71962ab5e864567da138817163
Sam Hu <samhuyc@gmail.com>
2023-03-11 22:36:54 -0500

    organization

---
+++ sandbox.py
@@ -0,0 +1,26 @@
+from typing import *
+from datamodel import *
+
+'''
+class Trader:
+
+    def run(self, state: TradingState) -> Dict[str, List[Order]]:
+        result: Dict[str, List[Order]]
+
+        if "Pearl
+
+        
+
+        
+
+        return result
+'''
+
+def main():
+
+    d = {0:1, 1:2, 2:4, 4:8}
+    for item in d:
+        print(item)
+
+    print(0 in d)
+main()
