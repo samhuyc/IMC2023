ff8b75b33becf3d026579a677598189f21aa8c50
Sam Hu <samhuyc@gmail.com>
2023-03-11 19:58:13 -0500

    finished python refresher video

--- test.py
+++ test.py
@@ -1,28 +1,44 @@
 ## This file is a test for python3
 
-
-
-def should_buy(product, price, valuations):
-    if product not in valuations:
-        return False
-    return valuations[product] > price
-
-
-def mystery_box_value(products, valuations):
-    box = {}
-    for key in products:
-        box[key] = products[key] * valuations[key]
+class Trade:
+    def __init__(self, price, volume):
+        self.price = price
+        self.volume = volume
     
-    return box
+    def is_buy(self):
+        return self.volume > 0
+
+    def __repr__(self) -> str:
+        return f"Trade ({self.volume} @ ${self.price})"
+
+class TradeTracker:
+    def __init__(self):
+        self.trades = []
+    
+    def add_trade(self, trade):
+        self.trades.append(trade)
+
+    def get_buy_trades(self):
+        buy_trades = []
+        for trade in self.trades:
+            if trade.volume > 0: 
+                buy_trades.append(trade)
+    
+        return buy_trades
+    
+    def average_traded_price(self):
+        total = 0
+        for trade in self.trades:
+            total += trade.price
+        
+        return total/len(self.trades)
 
 def main():
-    """
-    valuations = {"apple": 5, "banana": 10, "coconut": 8}
-    print(should_buy("apple", 10, valuations))
-    """
-    products = {'banana':200, 'shell':300, 'coconut':100}
-    valuations = {'banana':8, 'shell':4, 'coconut':13}
 
-    print(mystery_box_value(products,valuations))
-    
+    tracker = TradeTracker()
+    tracker.add_trade(Trade(7,4))
+    tracker.add_trade(Trade(10, -3))
+    print(tracker.get_buy_trades())
+    print(tracker.average_traded_price())
+
 main()
